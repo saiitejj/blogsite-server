cc3caa79e08d4750a559c787153485434c96b108
Sai Tej <ngvst2004@gmail.com>
2025-09-19 22:45:29 +0530

    feat:Add API route skel and env

--- main.go
+++ main.go
@@ -2,12 +2,25 @@ package main
 
 import (
 	"blogsite_server/routes"
+	"log"
+	"os" // for env variables
+
+
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main(){
+	err:=godotenv.Load()
+	if err!=nil{
+		log.Fatal("Error loading .env file")
+	}
 	router:=gin.Default()
 
 	routes.SetupRoutes(router)
-	router.Run(":8000")
+	port:=os.Getenv("PORT")
+	if port==""{
+		port="8080"
+	}
+	router.Run(":"+port)
 }
